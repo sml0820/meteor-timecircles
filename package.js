4b9e11eb76d8bc9f696417b780261d687aa1782d
Sean Lanning <sml0820@gmail.com>
2014-10-11 13:50:20 -0400

    update readme and add jquery dependency

--- package.js
+++ package.js
@@ -5,12 +5,13 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.3.1');
+  api.use('jquery');
+  api.versionsFrom('METEOR@0.9.0');
   api.addFiles(['TimeCircles/inc/TimeCircles.js', 'TimeCircles/inc/TimeCircles.css']);
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
+  api.use('tinytest, jquery');
   api.use('sml0820:meteor-timecircles');
   api.addFiles(['TimeCircles/inc/TimeCircles.js', 'TimeCircles/inc/TimeCircles.css']);
 });
